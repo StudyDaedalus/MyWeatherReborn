2312fb406574096034488fc10259f2c1c1149fdd
StudyDaedalus <867151939@qq.com>
2018-12-06 10:59:11 +0800

    2018.12.6-1

---
+++ app/src/main/java/com/blessingsoftware/myweatherreborn/AreaFragment.java
@@ -0,0 +1,207 @@
+package com.blessingsoftware.myweatherreborn;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blessingsoftware.myweatherreborn.DB.City;
+import com.blessingsoftware.myweatherreborn.DB.County;
+import com.blessingsoftware.myweatherreborn.DB.Province;
+import com.blessingsoftware.myweatherreborn.uitls.GsUtil;
+import com.blessingsoftware.myweatherreborn.uitls.HttpUtil;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AreaFragment extends Fragment {
+    //定义三层级别，越小优先级越高
+    public static final int level_province=0;
+    public static final int level_city=1;
+    public static final int level_county=2;
+
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String>dataList=new ArrayList<>();
+    //省市县列表和被选中的
+    private List<Province> provinceList;
+    private List<City>cityList;
+    private List<County>countyList;
+    private Province selectedProvince;
+    private City selectedCity;
+    //判断当前选中选项的级别
+    private int currentlevel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.choose_area,container,false);
+        titleText=(TextView)view.findViewById(R.id.title_text);
+        backButton=(Button)view.findViewById(R.id.back_button);
+        listView=(ListView)view.findViewById(R.id.list_view);
+        adapter=new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(currentlevel==level_province){
+                    selectedProvince=provinceList.get(position);
+                    queryCities();
+                }else if(currentlevel==level_city){
+                    selectedCity=cityList.get(position);
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentlevel==level_county){
+                    queryCities();
+                }else if(currentlevel==level_city){
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+        private void queryProvinces(){
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if(provinceList.size()>0){
+            dataList.clear();
+            for(Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentlevel=level_province;
+        }else{
+            String address="http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
+    private void queryCities(){
+        titleText.setText(selectedProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList = DataSupport.where("provinceid=?",String.valueOf(selectedProvince.getId()))
+                .find(City.class);
+        if(cityList.size()>0){
+            dataList.clear();
+            for(City city:cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentlevel=level_city;
+        }else{
+            int provinceCode= selectedProvince.getProvinceCode();
+            String address="http://guolin.tech/api/china/"+provinceCode;
+            queryFromServer(address,"city");
+        }
+    }
+    private void queryCounties(){
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList=DataSupport.where("cityid=?",String.valueOf(selectedCity.getId()))
+                .find(County.class);
+        if(countyList.size()>0){
+            dataList.clear();
+            for(County county:countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentlevel=level_county;
+        }else{
+            int provinceCode=selectedProvince.getProvinceCode();
+            int cityCode=selectedCity.getCityCode();
+            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
+            queryFromServer(address,"county");
+        }
+    }
+    private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败!",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText=response.body().string();
+                boolean result=false;
+                if("province".equals(type)) {
+                    result = GsUtil.ProvinceResponse(responseText);
+                }else if("city".equals(type)) {
+                    result = GsUtil.CityResponse(responseText,selectedProvince.getId());
+                }else if("county".equals(type)){
+                    result=GsUtil.CountyResponse(responseText,selectedCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }else if("city".equals(type)){
+                                queryCities();
+                            }else if("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void showProgressDialog(){
+        if(progressDialog==null){
+            progressDialog=new ProgressDialog(getActivity());
+            progressDialog.setMessage("加载中...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+
+    private void closeProgressDialog(){
+        if(progressDialog!=null){
+            progressDialog.dismiss();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/blessingsoftware/myweatherreborn/GSON/AQI.java
@@ -0,0 +1,9 @@
+package com.blessingsoftware.myweatherreborn.GSON;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
